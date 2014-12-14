937964af5ebcdd764b020635e86a4de504c09162
Yuji Imamura <imamura@imarketsceneviewer.com>
2014-12-14 22:10:54 +0900

    AutoLayout検証クラスを追加

---
+++ macTechnicalness/iOSApp/Controller/ViewController/iOSMTDemystifyAutoLayout1ViewController.m
@@ -0,0 +1,37 @@
+//
+//  iOSMTDemystifyAutoLayout1ViewController.m
+//  macTechnicalness
+//
+//  Created by Yuji Imamura on 2014/12/14.
+//
+//
+
+#import "iOSMTDemystifyAutoLayout1ViewController.h"
+
+@interface iOSMTDemystifyAutoLayout1ViewController ()
+
+@end
+
+@implementation iOSMTDemystifyAutoLayout1ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
