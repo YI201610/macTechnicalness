a76dcea59981bbea828f66607d10956a572f242a
Yuji Imamura <imamura@imarketsceneviewer.com>
2015-01-04 09:23:55 +0900

    Auto Layout検証用のWindowを調整中

--- macTechnicalness/OSXApp/Controller/WindowController/OSXMTAutoLayout1WindowController.m
+++ macTechnicalness/OSXApp/Controller/WindowController/OSXMTAutoLayout1WindowController.m
@@ -45,6 +45,18 @@
     [super windowDidLoad];
     
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    
+    /*!
+     @comment   width, heightをもつViewを生成する
+     */
+    //NSView* someView = [[NSView alloc] initWithFrame:CGRectMake(5, 5, 30.0f, 30.0f)];
+//    [someView setBackgroundColor:[UIColor greenColor]];
+//    someView.tag = 999;
+//    [someView addSubview:someView];
+    
+
+    
+    
 }
 
 /*!
