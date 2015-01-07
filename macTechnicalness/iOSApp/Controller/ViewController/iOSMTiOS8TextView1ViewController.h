32a922234b45edc5fa61ed542d72e7f77794bbe4
Yuji Imamura <imamura@imarketsceneviewer.com>
2015-01-08 00:52:12 +0900

    iOS8のTextView挙動を検証中

---
+++ macTechnicalness/iOSApp/Controller/ViewController/iOSMTiOS8TextView1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  iOSMTiOS8TextView1ViewController.h
+//  macTechnicalness
+//
+//  Created by Yuji Imamura on 2015/01/08.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iOSMTiOS8TextView1ViewController : UIViewController <UITextViewDelegate>
+
+@end
