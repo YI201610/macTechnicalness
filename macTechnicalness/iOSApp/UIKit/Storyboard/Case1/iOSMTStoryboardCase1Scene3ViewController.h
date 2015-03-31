73d3f505331d04d4e2e4d49d6f88e73b42138962
Yuji Imamura <imamura@imarketsceneviewer.com>
2015-04-01 08:03:15 +0900

    Storyboard, Scene, Segue　検証中

---
+++ macTechnicalness/iOSApp/UIKit/Storyboard/Case1/iOSMTStoryboardCase1Scene3ViewController.h
@@ -0,0 +1,13 @@
+//
+//  iOSMTStoryboardCase1Scene3ViewController.h
+//  macTechnicalness
+//
+//  Created by Yuji Imamura on 2015/04/01.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iOSMTStoryboardCase1Scene3ViewController : UITabBarController
+
+@end
