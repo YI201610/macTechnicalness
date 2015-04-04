ab23b7799c53269ab39b663143fa3f07e34337c4
Yuji Imamura <imamura@imarketsceneviewer.com>
2015-04-04 10:26:54 +0900

    Scene2からScene3に遷移するセグエの検証

--- macTechnicalness/iOSApp/UIKit/Storyboard/Case1/iOSMTStoryboardCase1Scene2ViewController.m
+++ macTechnicalness/iOSApp/UIKit/Storyboard/Case1/iOSMTStoryboardCase1Scene2ViewController.m
@@ -24,15 +24,37 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - UITableViewDataSource
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+//セクション数を返す
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
 }
-*/
+
+//各セクションの項目数を返す
+- (NSInteger)tableView:(UITableView*)tableView numberOfRowsInSection:(NSInteger)section {
+    return 30;
+}
+
+- (UITableViewCell*)tableView:(UITableView*)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    //	debugout(@"cell for row at IndexPath: (%d, %d)", indexPath.section, indexPath.row);
+
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"scene2Cell"];
+
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    UITableViewCell* cell = [tableView cellForRowAtIndexPath:indexPath];
+    [self performSegueWithIdentifier:@"gotoScene3" sender: cell];
+    
+}
 
 
 
