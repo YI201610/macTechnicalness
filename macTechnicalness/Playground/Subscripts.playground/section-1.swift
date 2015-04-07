efadfaa81af8e2455e295a00eae6cdb7f372d997
Yuji Imamura <imamura@imarketsceneviewer.com>
2015-04-08 01:14:54 +0900

    作業中

--- macTechnicalness/Playground/Subscripts.playground/section-1.swift
+++ macTechnicalness/Playground/Subscripts.playground/section-1.swift
@@ -134,40 +134,43 @@ var 材料データ = ["トマト": 8, "きゅうり": 6, "ひき肉": 4]
 
 
 
-struct Matrix {
+struct 行列 {
+    let 行: Int, 列: Int
+    var グリッド: [Double]  //行列の各成分を１列に並べて保持する
     
-    let rows: Int, columns: Int
-    var grid: [Double]
-    
-    init(rows: Int, columns: Int) {
-        self.rows = rows
-        self.columns = columns
-        grid = Array(count: rows * columns, repeatedValue: 0.0)
+    init(行: Int, 列: Int) {
+        self.行 = 行
+        self.列 = 列
+        グリッド = Array(count: 行 * 列, repeatedValue: 0.0)
     }
     
-    func indexIsValidForRow(row: Int, column: Int) -> Bool {
-        return row >= 0 && row < rows && column >= 0 && column < columns
+    func 成分の有効判定(行数: Int, 列数: Int) -> Bool {
+        return 行数 >= 0 && 行数 < 行 && 列数 >= 0 && 列数 < 列
     }
     
-    subscript(row: Int, column: Int) -> Double {
+    subscript(行: Int, 列: Int) -> Double {
         get {
-            assert(indexIsValidForRow(row, column: column), "Index out of range")
-            return grid[(row * columns) + column]
+            assert(成分の有効判定(行, 列数: 列), "範囲外")
+            return グリッド[(行 * 列) + 列]
         }
         set {
-            assert(indexIsValidForRow(row, column: column), "Index out of range")
-            grid[(row * columns) + column] = newValue
+            assert(成分の有効判定(行, 列数: 列), "範囲外")
+            グリッド[(行 * 列) + 列] = newValue
         }
     }
 }
 
-var matrix = Matrix(rows: 2, columns: 2)
-
-
+var matrix = 行列(行: 2, 列: 2)
+matrix.成分の有効判定(100, 列数: 100)
+matrix.成分の有効判定(1, 列数: 1)
 
 matrix[0, 1] = 1.5
 matrix[1, 0] = 3.2
-
+matrix[1, 1]
+matrix[1, 1] = 128.256
+matrix[1, 1]
+matrix[0, 0] = 1.3
+//matrix[20, 15] = 5
 
 
 //いかの文と、indexIsValidForRow2の関係
