d5c08dee7f6395615845a5f556a4e3e4b37099a2
Yuji Imamura <imamura@imarketsceneviewer.com>
2015-04-08 00:49:24 +0900

    作業中

--- macTechnicalness/Playground/Subscripts.playground/section-1.swift
+++ macTechnicalness/Playground/Subscripts.playground/section-1.swift
@@ -17,24 +17,81 @@ var str = "Hello, playground"
 //    // return an appropriate subscript value here
 //}
 
+struct someStructure {
+    
+    subscript(someValue: Int) -> Int {
+        
+        get {
+            
+            //何かの値を返す
+            return 0
+        }
+        
+        set(newValue){
+            //何かnewValueを使用して処理を行う
+        }
+    }
+}
+
+struct sampleA {
+    let prop1: Int
+    var prop2: Int
+    
+    subscript(value: Int) -> Int {
+        
+        get {
+            return prop1 + value
+        }
+        
+        set(newValue){
+           prop2 = newValue
+        }
+        
+    }
+}
+
+var sA = sampleA(prop1: 10, prop2: 20)
+sA[5]
+sA[5] = 500
+sA[5]
+sA.prop2
+
 
 struct TimesTable {
     
+    /*!
+    @abstract   乗数
+    */
     let multiplier: Int
     var hgoe: Int
+
     /*
-    @comment    サブスクリプトを定義している
+    @comment    サブスクリプトを定義している。サブスクリプトを定義できるのは、型につき、１つのみ。
     */
     subscript(index: Int) -> Int {
-        
+    
+        /*
+        @comment    getter
+        */
         get {
             return multiplier * index
         }
     
+        /*
+        @comment    setter
+        */
         set(newValue) {
-            hgoe *= 100
+            println("newValue: \(newValue), hgoe: \(self.hgoe)")
+            hgoe *= (newValue+100)
         }
     }
+    
+//    /*
+//    @comment    読み出し専用のサブスクリプトでは、getキーワードを省略できる。
+//    */
+//    subscript(hoge: Int) -> Int {
+//        return hoge*12
+//    }
 }
 
 /*
@@ -46,6 +103,8 @@ var threeTimesTable = TimesTable(multiplier: 3, hgoe: 5)
 @comment    サブスクリプトを介して値を取得
 */
 println("six times three is \(threeTimesTable[6])")
+
+threeTimesTable.hgoe
 
 /*
 @comment    サブスクリプトのsetterを使って値をつっこむ
@@ -60,25 +119,36 @@ threeTimesTable.hgoe
 
 
 
+/*
+@comment    辞書型の変数を作成
+*/
+var 材料データ = ["トマト": 8, "きゅうり": 6, "ひき肉": 4]
 
-var numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
-numberOfLegs["bird"] = 2
+/*
+@comment    辞書型の変数に、subscript形式でデータを登録している例
+*/
+材料データ["ピーマン"] = 2
+材料データ
 
 
 
 
 
 struct Matrix {
+    
     let rows: Int, columns: Int
     var grid: [Double]
+    
     init(rows: Int, columns: Int) {
         self.rows = rows
         self.columns = columns
         grid = Array(count: rows * columns, repeatedValue: 0.0)
     }
+    
     func indexIsValidForRow(row: Int, column: Int) -> Bool {
         return row >= 0 && row < rows && column >= 0 && column < columns
     }
+    
     subscript(row: Int, column: Int) -> Double {
         get {
             assert(indexIsValidForRow(row, column: column), "Index out of range")
@@ -89,11 +159,6 @@ struct Matrix {
             grid[(row * columns) + column] = newValue
         }
     }
-    
-//    func indexIsValidForRow2(row: Int, column: Int) -> Bool {
-//        return row >= 0 && row < rows && column >= 0 && column < columns
-//    }
-
 }
 
 var matrix = Matrix(rows: 2, columns: 2)
