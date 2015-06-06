d1e6c572dc175ce708dfd7f396e371c99caee01a
Yuji Imamura <imamura@imarketsceneviewer.com>
2015-06-06 12:07:24 +0900

    OS Xでmix and matchの件焼酎

---
+++ macTechnicalness/OSXApp/Controller/WindowController/OSXMTSwiftBridge1WindowController.m
@@ -0,0 +1,30 @@
+//
+//  OSXMTSwiftBridge1WindowController.m
+//  macTechnicalness
+//
+//  Created by Yuji Imamura on 2015/06/06.
+//
+//
+
+#import "OSXMTSwiftBridge1WindowController.h"
+#import "CommonHeader.h"
+
+@interface OSXMTSwiftBridge1WindowController ()
+
+@end
+
+@implementation OSXMTSwiftBridge1WindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+
+- (IBAction)didSelectActionButton:(id)sender {
+    _methodname_;
+    
+}
+
+@end
