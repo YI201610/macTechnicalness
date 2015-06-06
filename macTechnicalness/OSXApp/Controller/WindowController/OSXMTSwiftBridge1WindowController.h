9ad681df0a129c316d1323510dbf8d5c27e8e703
Yuji Imamura <imamura@imarketsceneviewer.com>
2015-06-06 10:10:04 +0900

    mix and match検証中

---
+++ macTechnicalness/OSXApp/Controller/WindowController/OSXMTSwiftBridge1WindowController.h
@@ -0,0 +1,13 @@
+//
+//  OSXMTSwiftBridge1WindowController.h
+//  macTechnicalness
+//
+//  Created by Yuji Imamura on 2015/06/05.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface OSXMTSwiftBridge1WindowController : NSWindowController
+
+@end
---
+++ macTechnicalness/OSXApp/Model/OSXMTSwiftBridge1/OSXMTSwiftBridge1Domain-Swift.h
@@ -0,0 +1,13 @@
+//
+//  OSXMTSwiftBridge1Domain-Swift.h
+//  macTechnicalness
+//
+//  Created by Yuji Imamura on 2015/06/05.
+//
+//
+
+#ifndef macTechnicalness_OSXMTSwiftBridge1Domain_Swift_h
+#define macTechnicalness_OSXMTSwiftBridge1Domain_Swift_h
+
+
+#endif
