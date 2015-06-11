93b4123c0b422bdbbe302de19c1f4923506d057d
Yuji Imamura <imamura@imarketsceneviewer.com>
2015-06-12 07:54:35 +0900

    コメントを追加

--- macTechnicalness/OSXApp/Controller/WindowController/OSXMTBlueTooth1WindowController.swift
+++ macTechnicalness/OSXApp/Controller/WindowController/OSXMTBlueTooth1WindowController.swift
@@ -8,6 +8,18 @@
 
 import Cocoa
 
+/*
+@comment    アップルのListerサンプルから引用
+
+To ensure that the `List` class
+is unarchivable from an instance that was archived in the Objective-C version of Lister, the `List` class
+declaration is annotated with @objc(AAPLList). This annotation ensures that the runtime name of the `List`
+class is the same as the `AAPLList` class defined in the Objective-C version of the app. It also allows
+the Objective-C version of Lister to unarchive a `List` instance that was archived in the Swift version.
+
+https://developer.apple.com/library/mac/samplecode/Lister/Listings/Swift_Common_List_swift.html
+
+*/
 @objc(OSXMTBlueTooth1WindowController)
 class OSXMTBlueTooth1WindowController: NSWindowController {
 
