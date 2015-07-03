3bab00222b3c2eca69fb0574b50ba0b1c0d40f30
Yuji Imamura <imamura@imarketsceneviewer.com>
2015-07-03 21:09:52 +0900

    項目だけ追加

---
+++ macTechnicalness/iOSApp/Controller/ViewController/iOSMTNetworkBackgroundFetch1ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  iOSMTNetworkBackgroundFetch1ViewController.swift
+//  macTechnicalness
+//
+//  Created by Yuji Imamura on 2015/07/03.
+//
+//
+
+import UIKit
+
+@objc(iOSMTNetworkBackgroundFetch1ViewController)
+class iOSMTNetworkBackgroundFetch1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
