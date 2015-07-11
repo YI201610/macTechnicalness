62c41982aab0fc7dbe16384dd7be6ad3fdc38ee9
Yuji Imamura <imamura@imarketsceneviewer.com>
2015-07-11 23:23:21 +0900

    コメント追記

---
+++ macTechnicalness/iOSApp/Controller/ViewController/iOSMTCase1PageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  iOSMTCase1PageViewController.swift
+//  macTechnicalness
+//
+//  Created by Yuji Imamura on 2015/07/11.
+//
+//
+
+import UIKit
+
+class iOSMTCase1PageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
