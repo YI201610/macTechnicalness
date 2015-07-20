3ff8f1c1080098962c7a00adb9c144875d095cbc
Yuji Imamura <imamura@imarketsceneviewer.com>
2015-07-20 20:58:35 +0900

    collectionView検証中

---
+++ macTechnicalness/iOSApp/View/iOSMTSample20150720CollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  iOSMTSample20150720CollectionViewCell.swift
+//  macTechnicalness
+//
+//  Created by Yuji Imamura on 2015/07/20.
+//
+//
+
+import UIKit
+
+@objc(iOSMTSample20150720CollectionViewCell)
+class iOSMTSample20150720CollectionViewCell: UICollectionViewCell {
+
+    
+    @IBOutlet weak var someLabel: UILabel!
+    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//        
+//        println("*collection view cell, awakeFromNib()...")
+//    }
+
+}
