1e18ace7dc28e065f33eaa6a5d8dac663e1c5b9c
Yuji Imamura <YI201610@users.noreply.github.com>
2018-04-03 05:22:25 +0900

    検証項目を追加

---
+++ macTechnicalness/iOSApp/UIKit/Verification20180403ViewController.h
@@ -0,0 +1,12 @@
+//
+//  Verification20180403ViewController.h
+//  iOSApp
+//
+//  Created by 今村裕治 on 2018/04/03.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Verification20180403ViewController : UIViewController
+
+@end
---
+++ macTechnicalness/iOSApp/UIKit/Verification20180403ViewController.m
@@ -0,0 +1,24 @@
+//
+//  Verification20180403ViewController.m
+//  iOSApp
+//
+
+#import "Verification20180403ViewController.h"
+
+@interface Verification20180403ViewController ()
+
+@end
+
+@implementation Verification20180403ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)didSelectButton:(UIButton *)sender {
+    
+}
+
+
+@end
